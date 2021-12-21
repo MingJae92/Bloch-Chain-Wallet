a069eb0e3b644f3f63d40ce939f8f6e3b15fecc1
Ming Chi <wong_mingchi@yahoo.co.uk>
2021-12-21 21:52:16 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import CurrencyConverter from './CurrencyConverter';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <CurrencyConverter />
+    </div>
+  );
+}
+
+export default App;
