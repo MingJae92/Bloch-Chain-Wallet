21cb321f6fc7e74fcc65044e0920a3e9145c2a10
Ming Chi <wong_mingchi@yahoo.co.uk>
2021-12-30 04:05:17 +0000

    PopUp updated

--- src/Modal.js
+++ src/Modal.js
@@ -1,21 +1,23 @@
 import React from 'react'
 import "./PopUp.css"
 
-const Modal =({setModalOpen})=> {
+const Modal =()=> {
   return (
     <div className="modal-wrapper">
       <h1>Wallet details</h1>
       {console.log("Hello, its working!")}
-      <button onClick={()=>{setModalOpen(false)}}>Close Wallet</button>
+      {/* <button onClick={()=>{setModalOpen(false)}}>Close Wallet</button> */}
         <div className="modal-header">
           <p>Account Balance</p>
         </div>
         <div className="modal-content">
           <div className='modal-body'>
             <h1>Account Balance</h1>
+            <h1>Chain ID</h1>
+            <h1>Balance</h1>
           </div>
           <div className='modal-footer'>
-            <button>Close</button>
+            <button>Disconnect</button>
           </div>
         </div>
     </div>
